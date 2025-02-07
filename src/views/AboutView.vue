86effa53f55818c4af63c83e80160b4fc43e5ce0
wksnders <42388072+wksnders@users.noreply.github.com>
2025-02-06 22:05:24 -0700

    more goats on the about page!

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,7 @@
 	<div class="about">
 		<h1>This is an about page</h1>
 		<p>goats are here now! they've taken over the about page and are beginning their conquest!</p>
+		<img src="https://i.imgur.com/FwnVqcr.jpeg" />
 	</div>
 </template>
 
